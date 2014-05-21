972e34d38cb7103c23309967e021d738d21df0e8
Patrick Keaveny <keavenyp@gmail.com>
2014-05-21 12:50:55 +0200

    Clean-up
    Added documentation to main index page, fixed indentation

--- index.php
+++ index.php
@@ -2,27 +2,25 @@
 
 include('includes/header.inc.php'); ?>
 	
-		<div id="main" class="row">
-				<div id="display-problems" class="col-lg-12" >
-					<div class=" button-group" id="problem-buttons"></div><!--#problem-buttons--> 
-					<table class="table"> 
-						<thead>
-							<tr>
-								<th class="hidden">id</th>
-								<th class="hidden">TimeStamp</th>
-								<th class="hidden">IP</th>
-								<th>Posted on:</th>
-								<th>Problem</th>
-								<th>Twitter</th>
-								<th>Contact User</th>
-								<!--<th>Delete Problem</th>-->
-							</tr>
-						</thead>
-						<tbody id="main-table"> 
-						</tbody>
-					</table>
-				</div><!--#display-problems-->
-			</div><!--/.col-lg-12-->
-		</div><!--/.row-fluid-->
+	<div id="main" class="row">
+			<div id="display-problems" class="col-lg-12" >
+				<div class=" button-group" id="problem-buttons"></div><!--extra features will be loaded here via AJAX --> 
+				<table class="table"> 
+					<thead>
+						<tr>
+							<th class="hidden">id</th>
+							<th class="hidden">TimeStamp</th>
+							<th class="hidden">IP</th>
+							<th>Posted on:</th>
+							<th>Problem</th>
+							<th>Twitter</th>
+							<th>Contact User</th>
+						</tr>
+					</thead>
+					<tbody id="main-table"></tbody><!-- the problems will be loaded here via AJAX -->
+				</table>
+			</div><!--#display-problems-->
+		</div><!--/.col-lg-12-->
+	</div><!--/.row-fluid-->
 		
-	<?php include('includes/footer.php');
+<?php include('includes/footer.php');
